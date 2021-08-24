44e74a8926e0f026b1af5fa4da9f1f5cf26ebc6e
somedaycode <somedaycode@gmail.com>
2021-08-24 23:18:36 +0900

    feat: 라우터 클래스 추가

---
+++ src/core/Router.js
@@ -0,0 +1,25 @@
+// 추후 ts로 변경
+export default class Router {
+  constructor(routes) {
+    this.routes = routes;
+  }
+
+  init(el) {
+    this.renderHTML(el, this.routes["/"]);
+    window.addEventListener("popstate", () =>
+      this.renderHTML(el, this.routes[location.pathname])
+    );
+  }
+
+  push(el, pathName) {
+    window.onpopstate = e => {
+      return this.renderHTML(el, this.routes[location.pathname]);
+    };
+    window.history.pushState({}, pathName, location.origin + pathName);
+    this.renderHTML(el, this.routes[pathName]);
+  }
+
+  renderHTML(el, route) {
+    new route(el);
+  }
+}
